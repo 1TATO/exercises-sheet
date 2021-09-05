62a850c87f644cf97b316050cd7709cc533e1d9c
José Neto <josenetopr5@gmail.com>
2021-09-05 15:28:58 -0300

    First routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,37 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello 123' });
+app.use(express.json());
+
+app.get('/dashboard', (req, res) => {
+  return res.json([
+    'Supino'
+  ]);
+});
+
+app.post('/dashboard', (req, res) => {
+  const params = request.body;
+
+  return res.json([
+    'Supino',
+    'Biceps'
+  ])
+});
+
+app.put('/dashboard/:id', (req, res) => {
+  const { id } = req.params;
+  console.log(id)
+
+  return res.json([
+    'Perna',
+    'Biceps'
+  ])
+});
+
+app.delete('/dashboard/:id', (req, res) => {
+  return res.json([
+    'Supino',
+  ])
 });
 
 app.listen(3333, () => {
