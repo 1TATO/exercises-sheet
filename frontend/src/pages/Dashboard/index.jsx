8599c1a0c474c8cce2bec404f055ec6c8db3a0d6
José Neto <josenetopr5@gmail.com>
2021-09-06 20:50:12 -0300

    Dashboard page

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,11 +1,69 @@
+import { FiEdit, FiTrash2 } from 'react-icons/fi';
+
 import Header from '../../components/Header';
+import Button from '../../components/Button';
+
+import { Container, Content } from './styles';
 
 function Dashboard() {
   return (
     <>
       <Header />
 
-      <h1>Dashboard</h1>
+      <Container>
+        <Button>Novo exercício</Button>
+
+        <Content>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Carga</th>
+                <th>Repetições</th>
+                <th className="action">Editar</th>
+                <th className="action">Excluir</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Biceps alternado</td>
+                <td>20</td>
+                <td>4X15</td>
+                <td className="action">
+                  <FiEdit />
+                </td>
+                <td className="action">
+                  <FiTrash2 />
+                </td>
+              </tr>
+              <tr>
+                <td>Supino</td>
+                <td>20</td>
+                <td>4X15</td>
+                <td className="action">
+                  <FiEdit />
+                </td>
+                <td className="action">
+                  <FiTrash2 />
+                </td>
+              </tr>
+              <tr>
+                <td>Supino</td>
+                <td>20</td>
+                <td>4X15</td>
+                <td className="action">
+                  <FiEdit />
+                </td>
+                <td className="action">
+                  <FiTrash2 />
+                </td>
+              </tr>
+
+            </tbody>
+          </table>
+        </Content>
+      </Container>
     </>
   );
 }
---
+++ frontend/src/pages/Dashboard/styles.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1120px;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+export const Content = styled.div`
+  margin-top: 4rem;
+
+  table {
+    width: 100%;
+    border-spacing: 0 0.5rem;
+    border: 2px solid var(--button);
+    color: var(--button);
+    border-radius: 0.5rem;
+
+    th {
+      padding: 1rem 2rem;
+      text-align: left;
+      line-height: 1.5rem;
+      font-weight: 600;
+
+      &.action {
+        width: 4rem;
+        text-align: center;
+      }
+    }
+
+    td {
+      padding: 1rem 2rem;
+
+      &.action {
+        text-align: center;
+        font-size: 1.25rem;
+      }
+    }
+  }
+`;
